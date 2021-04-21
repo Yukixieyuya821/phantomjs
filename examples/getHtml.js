7c963677d4e0b8770dea60bebfdae8a2649368c1
谢玉 <yuki.xie@happyeasygo.com>
2021-04-21 17:29:01 +0800

    获取html

--- examples/getHtml.js
+++ examples/getHtml.js
@@ -1,4 +1,12 @@
 var page = require('webpage').create();
-page.open('https://locallife.happyeasygo.com/hot-coupons-c0', function() {
-    
+page.open('https://locallife.happyeasygo.com/hot-coupons-c0', function(status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+      } else {
+        // var ua = page.evaluate(function() {
+        //   return document.getElementById('app').textContent;
+        // });
+        console.log(page.content);
+      }
+      phantom.exit();
 });
