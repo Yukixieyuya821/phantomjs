357405f6d31f7417ee727ab1e3ae7d7812c92ca4
谢玉 <yuki.xie@happyeasygo.com>
2021-04-21 16:56:21 +0800

    截屏js调整

--- examples/screenPage.js
+++ examples/screenPage.js
@@ -1,10 +1,5 @@
 var page = require('webpage').create();
-page.open('https://locallife.happyeasygo.com/hot-coupons-c0', function(status) {
-    console.log(123)
-    if (status !== 'success') {
-        console.log('Unable to access network');
-      } else {
-        console.log(document);
-      }
-      phantom.exit();
+page.open('https://locallife.happyeasygo.com/hot-coupons-c0', function() {
+    page.render('locallife.png');
+    phantom.exit();
 });
